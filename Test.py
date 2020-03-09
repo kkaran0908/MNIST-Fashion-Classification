305e46179a6b0df5c36dc9ea6def2c6dd4eab28b
karan kumar <36206827+kkaran0908@users.noreply.github.com>
2020-03-10 05:04:11 +0530

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,9 +1,26 @@
+import warnings
+warnings.filterwarnings("ignore")
 ##import all the required libraries
 import pickle
 import pandas as pd
 #reading the test data
-X_test = np.loadtxt('TestData.csv')
+Xtr = np.loadtxt('TestData.csv')
+#reshaping the image into 28*28 size
+images = []
+for img in Xtr:
+    images.append(img.reshape(28,28))
+#extracting the edge features from the images
+im_edge = []
+for im in images:
+    im_edge.append(feature.canny(im, sigma=5))
+
+#flatten the entire dataset
+images = []
+for im in im_edge:
+    images.append(im.flatten())
+images = np.array(images)
+
 #load the pretrained model
-model = pickle.load(open('xgboost.pickle', 'rb'))
-prediction = model.predict(X_test)
+model = pickle.load(open('LogisticReg.pickle', 'rb'))
+prediction = model.predict(images)
 np.savetxt("myPredictions.csv", prediction) 
--- Training.py
+++ Training.py
@@ -1,11 +1,29 @@
+import warnings
+warnings.filterwarnings("ignore")
 import pickle
-import xgboost as xgb
+from sklearn.linear_model import LogisticRegression as LR
 import numpy as np
+from skimage import feature
 ##read the dataset
-X_train=np.loadtxt("TrainData/TrainData.csv")
-y_train=np.loadtxt("TrainLabels.csv")
+Xtr=np.loadtxt("TrainData/TrainData.csv")
+Ytr=np.loadtxt("TrainLabels.csv")
+#reshaping the image into 28*28 size
+images = []
+for img in Xtr:
+    images.append(img.reshape(28,28))
+#extracting the edge features from the images
+im_edge = []
+for im in images:
+    im_edge.append(feature.canny(im, sigma=5))
+
+#flatten the entire dataset
+images = []
+for im in im_edge:
+    images.append(im.flatten())
+images = np.array(images)
 #train the model on the entire dataset
-clf = xgb.XGBClassifier(n_estimators = 150,max_depth = 100,n_jobs = -1)
-clf.fit(X_train,y_train)
+clf = LR(C=.1,n_jobs = -1)
+clf.fit(images,Ytr)
 ##save the model into the system
-pickle.dump(clf, open('xgboost.pickle', 'wb'))
+pickle.dump(clf, open('LogisticReg.pickle', 'wb'))
+print("Model Trained Successfully")
